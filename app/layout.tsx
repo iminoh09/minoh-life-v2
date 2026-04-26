acaee0e84daf0d791dbf12aad1de0b5d4f6badaf
iminoh09 <162568661+iminoh09@users.noreply.github.com>
2026-04-26 09:30:33 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,23 +38,17 @@ export default function RootLayout({
 }
 
 function Footer() {
-  const links = [
-    { name: 'github', url: 'https://github.com/iminoh09' }
-  ];
   return (
     <footer className="mt-12 text-center">
       <div className="flex justify-center space-x-4 tracking-tight">
-        {links.map((link) => (
-          
-            key={link.name}
-            href={link.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-400 dark:text-gray-500 hover:text-blue-500 transition-colors duration-200"
-          >
-            {link.name}
-          </a>
-        ))}
+        <a
+          href="https://github.com/iminoh09"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-400 dark:text-gray-500 hover:text-blue-500 transition-colors duration-200"
+        >
+          github
+        </a>
       </div>
     </footer>
   );
